83020d57da3e11e7d62175a1ef936914da44887b
DESKTOP-7E8ENMV\moish <moises.dev08@gmail.com>
2021-03-01 20:51:11 -0300

    feature/aboutUs component

---
+++ src/features/aboutUs/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div className="aboutUs">
+      <h3>This is the About Us component</h3>
+    </div>
+  );
+};
+
+export default AboutUs;
