14aa2795ee22b54c5ddc77b3e6dd3a638d9b23e3
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-03-02 23:29:53 -0300

    feature: Add new base footer

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-dark">
+                <div className="container">
+                    <div className="row">
+                        Footer
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
