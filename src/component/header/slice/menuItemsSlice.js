af80f4c5c348b6561980921f9b579281f22f1409
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-03 11:14:10 -0300

    feat: create basic slice for menu items

---
+++ src/component/header/slice/menuItemsSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const menuItemsSlice = createSlice({
+    name: "menuItems",
+    initialState: {},
+
+    reducers: {},
+});
+
+export default menuItemsSlice.reducer;
