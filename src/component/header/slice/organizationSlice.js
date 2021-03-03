6e002b0e3e840e1d9fcd3d43090962618f08a75b
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-03 11:09:35 -0300

    feat: create basic slice for the logo

---
+++ src/component/header/slice/organizationSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const organizationSlice = createSlice({
+    name: "organization",
+    initialState: {},
+
+    reducers: {},
+});
+
+export default organizationSlice.reducer;
