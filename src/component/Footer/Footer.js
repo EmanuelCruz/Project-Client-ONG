29ece7bdeae927dada87bfe23829760ec6d15217
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-03-03 10:59:01 -0300

    Fix: modularized info into new components.

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import CompanyInfo from './CompanyInfo'
+import Copyright from './Copyright'
 import './Footer.css'
+import SocialMedia from './SocialMedia'
+import WebLinks from './WebLinks'
 
 function Footer({ companyName, webLinks }) {
   return (
@@ -7,29 +11,11 @@ function Footer({ companyName, webLinks }) {
       <footer>
         <div class="container">
           <div class="row">
-            <div class="col-md-4 column item">
-              <h3>{companyName}</h3>
-              <a href="/home">
-                <img className="company-logo" src="https://www.freepnglogos.com/uploads/vodafone-png-logo/vodafone-logo-clarify-business-development-16.png" alt="" />
-              </a>
-            </div>
-            <div class="col-md-4 column item">
-              <h3>Links</h3>
-              <ul>
-                {webLinks.map(webLink => (
-                  <li><a href="#">{webLink}</a></li>
-                ))}
-              </ul>
-            </div>
-            <div class="col-md-4 column item">
-              <h3>¡Seguinos en nuestras redes!</h3>
-              <div class="col item social">
-                <a href="#"><i class="bi bi-facebook"></i></a><a href="#"><i class="bi bi-twitter"></i></a>
-                <a href="#"><i class="bi bi-linkedin"></i></a><a href="#"><i class="bi bi-instagram"></i></a>
-              </div>
-            </div>
+            <CompanyInfo companyName={companyName} />
+            <WebLinks webLinks={webLinks} />
+            <SocialMedia />
           </div>
-          <p class="copyright">ONG Alkemy © 2021</p>
+          <Copyright />
         </div>
       </footer>
     </div>
