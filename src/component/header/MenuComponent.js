986a548548b4cbed0f4279ab49752db9e6c5ca45
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-03 11:34:58 -0300

    feat: get initial state data

--- src/component/header/MenuComponent.js
+++ src/component/header/MenuComponent.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { itemsSelector } from "./slice/menuItemsSlice";
 
 export default function MenuComponent() {
-    return <div>items</div>;
+    const items = useSelector(itemsSelector);
+
+    console.log("Items: ", items);
+    return (
+        <div>
+            <ul>
+                <li>{items[0].texto}</li>
+                <li>{items[1].texto}</li>
+                <li>{items[2].texto}</li>
+                <li>{items[3].texto}</li>
+                <li>{items[4].texto}</li>
+            </ul>
+        </div>
+    );
 }
