0cd88167ee6f25457298a3456b2e36d07d5d67d9
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-03 11:28:38 -0300

    feat: creating header component

---
+++ src/component/header/HeaderComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import LogoComponent from "./LogoComponent";
+import MenuComponent from "./menuComponent";
+
+const UsersList = () => {
+    return (
+        <div>
+            <div>
+                <LogoComponent></LogoComponent>
+                <MenuComponent></MenuComponent>
+            </div>
+        </div>
+    );
+};
+
+export default UsersList;
