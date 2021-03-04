ad48809bd37508394fe82f8718fc9ee8a6322131
jhosep98 <jhosep149@hotmail.com>
2021-03-04 12:10:47 -0300

    feature/implementation of the contact form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
-import HeaderScreen from "./screen/header/HeaderScreen";
-import AboutUsScreen from "./screen/aboutUs/AboutUsScreen";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
+import React from 'react';
+import HeaderScreen from './screen/header/HeaderScreen';
+import AboutUsScreen from './screen/aboutUs/AboutUsScreen';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import { ContactScreen } from './screen/contact/ContactScreen';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/contacto">
+            <ContactScreen />
+          </Route>
           <Route path="/nosotros">
             <AboutUsScreen />
           </Route>
---
+++ src/screen/contact/ContactScreen.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ContactComponent } from '../../component/Contact/ContactComponent';
+
+export const ContactScreen = () => {
+  return <ContactComponent />;
+};
