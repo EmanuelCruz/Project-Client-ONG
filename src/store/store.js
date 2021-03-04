d905892feb62a8a7f47528481a458d39ae706346
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-04 15:54:36 -0300

    fix: delete code and files

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import menuItemsSlice from "../component/header/slice/menuItemsSlice";
-import organizationSlice from "../component/header/slice/organizationSlice";
-
-const store = configureStore({
-    reducer: { menuItems: menuItemsSlice, organization: organizationSlice },
-});
-
-export default store;
