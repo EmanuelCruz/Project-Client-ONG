e8983287351927c6694c28639c3f92ca04aff1b7
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-03 21:42:48 -0300

    fix: delete import that is not used and simplify code

--- src/component/header/MenuComponent.js
+++ src/component/header/MenuComponent.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchItems, itemsSelector } from "./slice/menuItemsSlice";
-import { Link } from "react-router-dom";
 
 export default function MenuComponent() {
     const dispatch = useDispatch();
@@ -14,17 +13,7 @@ export default function MenuComponent() {
     const renderItems = () => {
         if (loading) return <p>Loading items...</p>;
         if (hasErrors) return <p>Cannot display items...</p>;
-
-        return items.map((item, index) => {
-            let to = item.ruta;
-            return (
-                <div key={index}>
-                    <Link className="" to={to}>
-                        {item.texto}
-                    </Link>
-                </div>
-            );
-        });
+        return items.map((item, index) => <div key={index}>{item.texto}</div>);
     };
 
     return (
