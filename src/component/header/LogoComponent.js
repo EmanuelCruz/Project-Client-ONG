0b425c974cd8a7b6e3a1a556d43630b316492d62
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-03 21:39:27 -0300

    fix: delete import that is not used

--- src/component/header/LogoComponent.js
+++ src/component/header/LogoComponent.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData, organizationSelector } from "./slice/organizationSlice";
-import { Link } from "react-router-dom";
 
 export default function LogoComponent() {
     const dispatch = useDispatch();
