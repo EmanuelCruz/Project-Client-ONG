8325d7ba49c31bf5327a7c2a3abdb9ec05542fc6
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-03-05 15:17:15 -0300

    Fix: Update slider component

--- src/component/Slider/SliderComponent.js
+++ src/component/Slider/SliderComponent.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import { SliderImagesData } from './SliderImagesData'
-import SliderLogic from './SliderLogic'
+import React, { useEffect, useState } from 'react'
+import { sliderImages } from './SliderImagesData'
+import Arrows from './Arrows'
+import Dots from './Dots'
+import SliderContent from './SliderContent'
+import './slider.css'
+
+const slidersLength = sliderImages.length - 1
 
 function SliderComponent() {
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex(activeIndex === slidersLength ? 0 : activeIndex + 1)
+    }, 5000)
+    return () => clearInterval(interval)
+  }, [activeIndex])
+
   return (
-    <div>
-      <SliderLogic slides={SliderImagesData} />
+    <div className="slider-container">
+      <SliderContent activeIndex={activeIndex} />
+      <Arrows
+        prevSlide={() =>
+          setActiveIndex(activeIndex < 1 ? slidersLength : activeIndex - 1)}
+        nextSlide={() =>
+          setActiveIndex(activeIndex === slidersLength ? 0 : activeIndex + 1)}
+      />
+      <Dots activeIndex={activeIndex} handleClick={(activeIndex) => setActiveIndex(activeIndex)} />
+
     </div>
   )
 }
