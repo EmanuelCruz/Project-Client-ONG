3b24938ead6c931581a2ab56f428d7acdf018617
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-03-05 15:17:55 -0300

    Fix: Update sliders image data.

--- src/component/Slider/SliderImagesData.js
+++ src/component/Slider/SliderImagesData.js
@@ -1,11 +1,14 @@
-export const SliderImagesData = [
-  {
-    image: 'https://images.unsplash.com/photo-1606092195730-5d7b9af1efc5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-  },
-  {
-    image: 'https://images.unsplash.com/photo-1503676260728-1c00da094a0b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1909&q=80'
-  },
-  {
-    image: 'https://images.unsplash.com/photo-1533254012848-644c18f39289?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-  }
+export const sliderImages = [
+    {
+        urls: 'https://mdbootstrap.com/img/Photos/Slides/img%20(141).jpg',
+        title: 'Imagen 1',
+    },
+    {
+        urls: 'https://mdbootstrap.com/img/Photos/Slides/img%20(142).jpg',
+        title: 'Imagen 2',
+    },
+    {
+        urls: 'https://mdbootstrap.com/img/Photos/Slides/img%20(143).jpg',
+        title: 'Imagen 3',
+    }
 ]
