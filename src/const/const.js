90aac0e0cf1699518f1b570c6d268085a8ce820c
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-03-10 11:28:52 -0300

    Fix: Add new variables for EditOrganization.

--- src/const/const.js
+++ src/const/const.js
@@ -31,3 +31,16 @@ export const NAME_MIN_LENGTH_NUMBER = 2;
 export const LASTNAME_MIN_LENGTH_WARNING =
   'El apellido debe tener al menos 2 caracteres';
 export const LASTNAME_MIN_LENGTH_NUMBER = 2;
+
+// Form Edit Organization
+
+export const objectIsEmpty = (obj) => {
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key))
+      return false
+  }
+  return true;
+}
+
+export const LOGO_MIN_LENGTH_WARNING =
+  'Debes subir una imagen.'
