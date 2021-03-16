61cd5372dce841fd829a211e748c6c5bdee3e1a8
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-16 14:49:56 -0300

    Feat: Creating style file for the component

---
+++ src/component/NovedadesRecientes/StylesNovedadesRecientesComponente.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+const myStyles = makeStyles((theme) => ({
+    root: {
+        width: "100%",
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+export default myStyles;
