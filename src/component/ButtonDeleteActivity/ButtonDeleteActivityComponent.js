c608a60a676d7ea11d33132bf4272078b72feac4
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-19 20:52:17 -0300

    Feat: Update the click event function

--- src/component/ButtonDeleteActivity/ButtonDeleteActivityComponent.js
+++ src/component/ButtonDeleteActivity/ButtonDeleteActivityComponent.js
@@ -1,27 +1,31 @@
 import React from "react";
-import IconButton from '@material-ui/core/IconButton';
+import IconButton from "@material-ui/core/IconButton";
 import { Delete } from "@material-ui/icons";
-import {ConfirmAlertDeleteActivtyComponent} from '../Alert/AlertComponent'
-import {deleteActivity} from '../../services/querys/activitiesServices'
-import Swal from 'sweetalert2';
-import {CONFIRM,CONFIRM_IS_CONFIRMED,CONFIRM_SUCCESS} from '../../const/const'
+import { ConfirmAlertDeleteActivtyComponent } from "../Alert/AlertComponent";
+import { deleteActivity } from "../../services/querys/activitiesServices";
+import Swal from "sweetalert2";
+import {
+    CONFIRM,
+    CONFIRM_IS_CONFIRMED,
+    CONFIRM_SUCCESS,
+} from "../../const/const";
 
-export default function ButtonDeleteActivityComponent({ id }) {
-
-    const handleDeleteActivity = () => {
-        ConfirmAlertDeleteActivtyComponent(id)
-            .then((result) => {
-                if (result.isConfirmed) {
-                    deleteActivity(id);
-                    Swal.fire(CONFIRM, CONFIRM_IS_CONFIRMED, CONFIRM_SUCCESS);
-                }
-            })
-        ;
+function ButtonDeleteActivityComponent({ activity, handleDeleteActivity }) {
+    const handleClick = () => {
+        ConfirmAlertDeleteActivtyComponent().then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(CONFIRM, CONFIRM_IS_CONFIRMED, CONFIRM_SUCCESS);
+                deleteActivity(activity.id);
+                handleDeleteActivity(activity.id); //Just for testing
+            }
+        });
     };
 
     return (
-        <IconButton color="secondary" component="span" onClick={()=>handleDeleteActivity()}>
+        <IconButton color="secondary" component="span" onClick={handleClick}>
             <Delete />
         </IconButton>
     );
 }
+
+export default ButtonDeleteActivityComponent;
