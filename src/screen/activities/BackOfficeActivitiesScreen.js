0d545ac07a7a6525469a98e04eec383457a40a7b
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-19 20:57:23 -0300

    Fix: Import not used

--- src/screen/activities/BackOfficeActivitiesScreen.js
+++ src/screen/activities/BackOfficeActivitiesScreen.js
@@ -1,6 +1,5 @@
 import React, { useState ,useEffect } from 'react';
 import BackOfficeActivitiesList from "../../component/BackOfficeActivities/BackOfficeActivitiesList";
-import {ACTIVITIES} from '../../const/const'
 
 function BackOfficeActivitiesScreen() {
 
