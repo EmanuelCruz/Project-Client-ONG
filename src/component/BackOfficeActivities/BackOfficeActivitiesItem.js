1ac2aa549accd6e1a08c52fad26d60823064b729
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-19 14:28:25 -0300

    Feat: Modify the appearance of the button and add functionality

--- src/component/BackOfficeActivities/BackOfficeActivitiesItem.js
+++ src/component/BackOfficeActivities/BackOfficeActivitiesItem.js
@@ -2,7 +2,8 @@ import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import IconButton from '@material-ui/core/IconButton';
-import { Edit, Delete } from "@material-ui/icons";
+import { Edit } from "@material-ui/icons";
+import ButtonDeleteActivity from '../ButtonDeleteActivity/ButtonDeleteActivityComponent'
 
 function BackOfficeActivitiesItem({activity}) {
 
@@ -15,9 +16,7 @@ function BackOfficeActivitiesItem({activity}) {
                 <IconButton color="primary" component="span">
                     <Edit />
                 </IconButton>
-                <IconButton color="secondary" component="span">
-                    <Delete />
-                </IconButton>
+                <ButtonDeleteActivity id={activity.id}/>
             </TableCell>
         </TableRow>
     )
--- src/component/ButtonDeleteActivity/ButtonDeleteActivityComponent.js
+++ src/component/ButtonDeleteActivity/ButtonDeleteActivityComponent.js
@@ -1,20 +1,27 @@
 import React from "react";
-import DeleteIcon from "@material-ui/icons/Delete";
-import Button from "@material-ui/core/Button";
-import { myStyles } from "./ButtonDeleteActivityStyles";
+import IconButton from '@material-ui/core/IconButton';
+import { Delete } from "@material-ui/icons";
 import {ConfirmAlertDeleteActivtyComponent} from '../Alert/AlertComponent'
+import {deleteActivity} from '../../services/querys/activitiesServices'
+import Swal from 'sweetalert2';
+import {CONFIRM,CONFIRM_IS_CONFIRMED,CONFIRM_SUCCESS} from '../../const/const'
 
-export default function ButtonDeleteActivityComponent({ id = 1 }) {
-    const classes = myStyles();
+export default function ButtonDeleteActivityComponent({ id }) {
+
+    const handleDeleteActivity = () => {
+        ConfirmAlertDeleteActivtyComponent(id)
+            .then((result) => {
+                if (result.isConfirmed) {
+                    deleteActivity(id);
+                    Swal.fire(CONFIRM, CONFIRM_IS_CONFIRMED, CONFIRM_SUCCESS);
+                }
+            })
+        ;
+    };
+
     return (
-        <Button
-            variant="contained"
-            color="secondary"
-            className={classes.button}
-            startIcon={<DeleteIcon />}
-            onClick={()=>ConfirmAlertDeleteActivtyComponent(id)}
-        >
-            Delete
-        </Button>
+        <IconButton color="secondary" component="span" onClick={()=>handleDeleteActivity()}>
+            <Delete />
+        </IconButton>
     );
 }
