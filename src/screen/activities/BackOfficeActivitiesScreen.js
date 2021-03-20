a661fb7021309bd47d0c38bc57bc294a5af5fbf2
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-19 21:04:21 -0300

    Fix: Hook import not used

--- src/screen/activities/BackOfficeActivitiesScreen.js
+++ src/screen/activities/BackOfficeActivitiesScreen.js
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect } from 'react';
+import React, { useState } from 'react';
 import BackOfficeActivitiesList from "../../component/BackOfficeActivities/BackOfficeActivitiesList";
 
 function BackOfficeActivitiesScreen() {
