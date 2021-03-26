4a7bf681243b454f5f05af9eb9f73183133ee1b4
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-26 08:46:17 -0300

    Fix: Margin Header

--- src/screen/header/HeaderScreenStyles.js
+++ src/screen/header/HeaderScreenStyles.js
@@ -2,7 +2,6 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
     header: {
-        marginBottom: "64px",
         "@media (max-width: 600px)": {
             marginBottom: "56px",
         },
