b7825f88860df2b8077b1ad47bd7940a0dfb36ba
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-03-26 00:21:47 -0300

    Fix: Add bootstrap globally

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import store from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { CssBaseline } from "@material-ui/core";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
     <React.StrictMode>
