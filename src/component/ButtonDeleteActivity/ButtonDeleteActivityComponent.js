3e0e58a6448f902e9114c84d650e762d9d45d2df
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-28 21:25:29 -0300

    Fix: Update function

--- src/component/ButtonDeleteActivity/ButtonDeleteActivityComponent.js
+++ src/component/ButtonDeleteActivity/ButtonDeleteActivityComponent.js
@@ -1,18 +1,19 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
 import { Delete } from "@material-ui/icons";
-import { ConfirmAlertDeleteActivtyComponent } from "../Alert/AlertComponent";
+import { ConfirmAlertDeleteButtonComponent } from "../Alert/AlertComponent";
 import { deleteActivity } from "../../services/querys/activitiesServices";
 import Swal from "sweetalert2";
 import {
     CONFIRM,
     CONFIRM_IS_CONFIRMED,
     CONFIRM_SUCCESS,
+    DELETE_CONFIRM_TITLE_ACTIVITY,
 } from "../../const/const";
 
 function ButtonDeleteActivityComponent({ activity, handleDeleteActivity }) {
     const handleClick = () => {
-        ConfirmAlertDeleteActivtyComponent().then((result) => {
+        ConfirmAlertDeleteButtonComponent(DELETE_CONFIRM_TITLE_ACTIVITY).then((result) => {
             if (result.isConfirmed) {
                 Swal.fire(CONFIRM, CONFIRM_IS_CONFIRMED, CONFIRM_SUCCESS);
                 deleteActivity(activity.id);
