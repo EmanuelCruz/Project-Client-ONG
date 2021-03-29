d5952fec5c0607edc85882643088ca9a131e71bd
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-29 10:48:13 -0300

    Feat: Add requests to create and modify

--- src/services/querys/categoriesServices.js
+++ src/services/querys/categoriesServices.js
@@ -1,10 +1,34 @@
 import { SERVER_URL_CATEGORIES } from "../../const/const";
 import apiServices from "./apiServices";
+import axios from "axios";
 
 const getCategories = () => {
     return apiServices.get(SERVER_URL_CATEGORIES);
 };
 
+const createCategory = async (data) => {
+    try {
+        const response = await axios.post(SERVER_URL_CATEGORIES, data);
+        return response.data;
+    } catch (err) {
+        return err;
+    }
+};
+
+const updateCategory = async (data, id) => {
+    try {
+        const response = await axios.patch(
+            `${SERVER_URL_CATEGORIES}/${id}`,
+            data
+        );
+        return response.data;
+    } catch (err) {
+        return err;
+    }
+};
+
 export {
-    getCategories
+    getCategories,
+    createCategory,
+    updateCategory
 };
