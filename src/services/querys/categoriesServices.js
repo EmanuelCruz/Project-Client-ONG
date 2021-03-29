0448367e4407f2121f8da0aa245cd78cdfb1abc6
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-28 21:30:16 -0300

    Feat: Query to delete category

--- src/services/querys/categoriesServices.js
+++ src/services/querys/categoriesServices.js
@@ -1,10 +1,24 @@
-import { SERVER_URL_CATEGORIES } from "../../const/const";
+import axios from "axios";
+import { MAIN_URL, SERVER_URL_CATEGORIES } from "../../const/const";
 import apiServices from "./apiServices";
 
 const getCategories = () => {
     return apiServices.get(SERVER_URL_CATEGORIES);
 };
 
+
+const deleteCategory = async (id) => {
+    try {
+        const response = await axios.delete(
+            `${MAIN_URL}${SERVER_URL_CATEGORIES}/${id}`
+        );
+        return response.data;
+    } catch (err) {
+        return err;
+    }
+};
+
 export {
-    getCategories
+    getCategories,
+    deleteCategory
 };
