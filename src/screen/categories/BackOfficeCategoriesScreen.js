c0f06c2d9588abdf82a916411447816a67b2f1a4
EmanuelCruz <emanuelcruz091@gmail.com>
2021-03-28 21:21:08 -0300

    Feat: Refresh screen when a category is deleted

--- src/screen/categories/BackOfficeCategoriesScreen.js
+++ src/screen/categories/BackOfficeCategoriesScreen.js
@@ -10,10 +10,10 @@ function BackOfficeCategoriesScreen() {
         getCategories().then(res => {
             setCategories(res.data);
         }).catch(err => console.log(err.message));
-    }, []);
+    }, [categories]);
 
     return (
-        <div className="container mt-5 pt-5 w-25">
+        <div className="container mt-5 pt-5 w-50">
             <h1>Categories</h1>
             {categories.map((category) => (
                 <CategoriesCardComponent key={category.id} category={category} />
