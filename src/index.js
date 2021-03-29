54d19660bb361a73abc543959056ede40975541b
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-03-29 10:39:00 -0300

    Fix: Fix styles

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import store from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { CssBaseline } from "@material-ui/core";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import "bootstrap/dist/css/bootstrap.css";
 import theme from "./style/theme";
 
