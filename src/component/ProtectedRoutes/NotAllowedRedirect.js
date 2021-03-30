78d3ac1593586f8536f6bb39c53393556546ffb8
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-03-30 17:40:40 -0300

    Fix: Fix conflicts

---
+++ src/component/ProtectedRoutes/NotAllowedRedirect.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotAllowedRedirect = () => {
+  return (
+    <div>
+      <p>No tienes los permisos para ver esta sección</p>
+      <p>Serás redirigido al Home en 5 segundos...</p>
+      <p>Si no eres redirigido, haz click <Link to="/">aquí.</Link></p>
+    </div>
+  )
+}
+
+export default NotAllowedRedirect;
