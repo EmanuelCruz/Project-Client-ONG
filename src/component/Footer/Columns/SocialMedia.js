f536dcdaf7b6e8d33a298bafe1c949d53edf1aa7
jhosep98 <jhosep149@hotmail.com>
2021-04-02 16:46:07 -0300

    adding social links in the footer

--- src/component/Footer/Columns/SocialMedia.js
+++ src/component/Footer/Columns/SocialMedia.js
@@ -3,16 +3,12 @@ import { getOrganization } from "../../../services/querys/organizationServices";
 
 function SocialMedia({ id }) {
   let [facebookContact, setFacebookContact] = useState();
-  let [twitterContact, setTwitterContact] = useState();
-  let [linkedinContact, setLinkedinContact] = useState();
   let [instagramContact, setInstagramContact] = useState();
 
   useEffect(() => {
     async function fetchOrganizationContacts() {
       const response = await getOrganization(id);
       setFacebookContact(response.contactFacebook);
-      setTwitterContact(response.contactTwitter);
-      setLinkedinContact(response.contactLinkedin);
       setInstagramContact(response.contactInstagram);
     }
 
@@ -23,16 +19,18 @@ function SocialMedia({ id }) {
     <div className="col-md-4 column item">
       <h3>¡Seguinos en nuestras redes!</h3>
       <div className="col item social">
-        <a href="#">
+        <a
+          href="https://www.facebook.com/fundacionzonasgrises/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="bi bi-facebook">{facebookContact}</i>
         </a>
-        <a href="#">
-          <i className="bi bi-twitter">{twitterContact}</i>
-        </a>
-        <a href="#">
-          <i className="bi bi-linkedin">{linkedinContact}</i>
-        </a>
-        <a href="#">
+        <a
+          href="https://www.instagram.com/fundacionzonasgrises/?hl=es"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="bi bi-instagram">{instagramContact}</i>
         </a>
       </div>
