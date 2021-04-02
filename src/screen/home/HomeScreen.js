fdab8fef68df5a907ceb449d677101ba57a9128e
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-04-02 15:43:47 -0300

    Feature: Add and fix News at HomeScreen

--- src/screen/home/HomeScreen.js
+++ src/screen/home/HomeScreen.js
@@ -1,26 +1,17 @@
 import React, { useState } from "react";
 import SliderComponent from "./../../component/Slider/SliderComponent";
+import LastNews from "./News/LastNews";
+import News from "./News/News";
+import Welcome from "./Welcome/Welcome";
 
 const HomeScreen = () => {
-  const [welcomeHomeTitle, setWelcomeHomeTitle] = useState('welcomeTitle');
-  const [welcomeHomeMessage, setWelcomeHomeMessage] = useState('welcomeMessage');
-  const [news, setNews] = useState({
-    news: {
-      one: "",
-      two: "",
-      three: "",
-      four: "",
-    },
-  });
 
   return (
     <div className="home">
       <SliderComponent />
-      <h4>{welcomeHomeTitle}</h4>
-      <p>{welcomeHomeMessage}</p>
-      <h4>Últimas novedades</h4>
-      <h4>{news.three} Here goes the third new</h4>
-      <h4>{news.four} Here goes the fourth new</h4>
+      <Welcome />
+      <LastNews />
+      <News />
     </div>
   );
 };
