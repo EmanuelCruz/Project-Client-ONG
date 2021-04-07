a1e9406ec7df53a1b42397af9553b7954e75253b
jhosep98 <jhosep149@hotmail.com>
2021-04-07 15:55:37 -0300

    show contact list in backoffice

--- src/component/BackOfficeContacts/components/ContactsTable.js
+++ src/component/BackOfficeContacts/components/ContactsTable.js
@@ -2,14 +2,14 @@ import React from "react";
 import { TableCell, TableRow } from "@material-ui/core";
 
 const ContactsTable = ({ contactData }) => {
-  const { name, phone, email, message, deletedAt } = contactData;
+  const { name, phone, email, message, createdAt } = contactData;
   return (
     <TableRow>
       <TableCell>{name}</TableCell>
       <TableCell>{phone}</TableCell>
       <TableCell>{email}</TableCell>
       <TableCell>{message}</TableCell>
-      <TableCell>{new Date(deletedAt).toISOString().substring(0, 10)}</TableCell>
+      <TableCell>{new Date(createdAt).toISOString().substring(0, 10)}</TableCell>
     </TableRow>
   );
 };
