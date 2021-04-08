e883905092419a97dd1929e81a5a7a5159f2d937
EmanuelCruz <emanuelcruz091@gmail.com>
2021-04-08 19:51:33 -0300

    Fix: Merge

--- src/component/FormCategories/FormCategories.js
+++ src/component/FormCategories/FormCategories.js
@@ -2,65 +2,6 @@ import React, { useEffect, useState } from "react";
 import {
   createCategory,
   updateCategory,
-<<<<<<< HEAD
-  getACategory,
-} from "../../services/querys/categoriesServices";
-import { Button, TextField, Container } from "@material-ui/core/";
-import useStyles from "./FormCategoriesStyles";
-import { useParams } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import {
-  CreateCategorieSuccess,
-  UpdateCategorieSuccess,
-} from "../Alert/AlertComponent";
-
-export default function FormCategories() {
-  const classes = useStyles();
-  const [category, setCategory] = useState([]);
-  const [emptyFields, setEmptyFields] = useState(true);
-  let { id } = useParams();
-  let history = useHistory();
-
-  useEffect(() => {
-    if (id) {
-      const fetchCategories = async (id) => {
-        const categorie = await getACategory(id);
-        setCategory(categorie.data);
-      };
-      fetchCategories(id);
-    }
-  }, []);
-
-  useEffect(() => {
-    handleEmptyFields();
-  }, [category]);
-
-  const handleEmptyFields = () => {
-    if (!category.name || !category.description) {
-      setEmptyFields(true);
-    } else {
-      setEmptyFields(false);
-    }
-  };
-
-  const handleSubmit = () => {
-    if (!category.id) {
-      createCategory(category);
-      clearForm();
-      setTimeout(function () {
-        history.push("/backoffice/categories");
-      }, 3000);
-      CreateCategorieSuccess();
-    } else {
-      updateCategory(category, category.id);
-      setTimeout(function () {
-        history.push("/backoffice/categories");
-      }, 3000);
-      UpdateCategorieSuccess();
-    }
-  };
-
-=======
 } from "../../services/querys/categoriesServices";
 import { Button, TextField, Container } from "@material-ui/core/";
 import useStyles from "./FormCategoriesStyles";
@@ -101,7 +42,6 @@ export default function FormCategories({ dataCategory }) {
     }
   }
 
->>>>>>> a7fdbde9e8a3967eadda1cd8a9faf5268aa450e2
   const clearForm = () => {
     for (const property in category) {
       category[property] = "";
@@ -113,39 +53,6 @@ export default function FormCategories({ dataCategory }) {
   const changeHandler = (event) => {
     category[event.target.id] = event.target.value;
     setCategory({ ...category });
-<<<<<<< HEAD
-    console.log(category);
-  };
-
-  return (
-    <Container>
-      <form className={classes.root}>
-        <TextField
-          id="name"
-          label="Nombre"
-          value={"" || category.name}
-          onChange={changeHandler}
-        />
-        <TextField
-          id="description"
-          label="Descripción"
-          value={"" || category.description}
-          onChange={changeHandler}
-        />
-        <Button
-          className={classes.button}
-          variant="contained"
-          component="label"
-          onClick={handleSubmit}
-          disabled={emptyFields}
-        >
-          {!category.id ? "Crear" : "Modificar"}
-        </Button>
-      </form>
-    </Container>
-  );
-}
-=======
     event.target.id === "name" ? setModifiedName(true) : setModifiedDescription(true)
   };
 
@@ -179,4 +86,3 @@ export default function FormCategories({ dataCategory }) {
     </div>
   );
 };
->>>>>>> a7fdbde9e8a3967eadda1cd8a9faf5268aa450e2
