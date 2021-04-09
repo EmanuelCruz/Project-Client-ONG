8e2562dc2d8b65b796c9d705b63b52c3ffcc60e2
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-04-09 10:55:21 -0300

    Feature: Add isAdmin status

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../store/ducks/user";
 import isAuthReducer from "../store/isAuth/reducer";
+import isAdminReducer from "../store/isAdmin/reducer";
 
 export default configureStore({
   reducer: {
     user: userReducer,
     isAuth: isAuthReducer,
+    isAdmin: isAdminReducer,
   },
 });
---
+++ src/store/isAdmin/reducer.js
@@ -0,0 +1,16 @@
+const initialState = { isAdmin: false };
+
+// action es el valor devuelto por el action
+//action.payload será el valor que quiero añadir, borrar, etc
+export default (state = initialState, action) => {
+  if (action.type === 'LOGIN') {
+    return {
+      ...state, //Lo que devuelve un reducer es lo que se quedará en el state, por tanto, debe devolver todo lo que había antes (además de la información que cambia)
+      isAdmin: action.payload
+    }
+  }
+
+  return state;
+};
+
+export const updateIsAdminStatus = state => state.isAdminReducer.isAdmin;
