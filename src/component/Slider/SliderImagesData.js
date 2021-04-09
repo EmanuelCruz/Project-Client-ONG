b6a3d81cb46258dc3cc755500e13167dd974e662
jhosep98 <jhosep149@hotmail.com>
2021-04-09 12:07:37 -0300

    change images on slide and home text

--- src/component/Slider/SliderImagesData.js
+++ src/component/Slider/SliderImagesData.js
@@ -1,14 +1,17 @@
 export const sliderImages = [
-    {
-        urls: 'https://mdbootstrap.com/img/Photos/Slides/img%20(141).jpg',
-        title: 'Imagen 1',
-    },
-    {
-        urls: 'https://mdbootstrap.com/img/Photos/Slides/img%20(142).jpg',
-        title: 'Imagen 2',
-    },
-    {
-        urls: 'https://mdbootstrap.com/img/Photos/Slides/img%20(143).jpg',
-        title: 'Imagen 3',
-    }
-]
+  {
+    urls:
+      "https://alkemy-ong.s3.sa-east-1.amazonaws.com/image/1617977533200WhatsApp%20Image%202020-06-01%20at%2017.09.17.jpeg",
+    title: "Cuarentena Solidaria",
+  },
+  {
+    urls:
+      "https://alkemy-ong.s3.sa-east-1.amazonaws.com/image/1617979788398WhatsApp%20Image%202020-04-21%20at%2013.57.53.jpeg",
+    title: "Donaciones",
+  },
+  {
+    urls:
+      "https://alkemy-ong.s3.sa-east-1.amazonaws.com/image/1617980454920WhatsApp%20Image%202020-08-21%20at%2018.33.14.jpeg",
+    title: "Dia del niño",
+  },
+];
