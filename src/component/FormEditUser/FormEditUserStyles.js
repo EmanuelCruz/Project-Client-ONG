a290636e31eb3cdd751781003f2898153f328b89
EmanuelCruz <emanuelcruz091@gmail.com>
2021-04-13 10:44:55 -0300

    Feat: feat edit user and styles

---
+++ src/component/FormEditUser/FormEditUserStyles.js
@@ -0,0 +1,30 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        height: "65vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    containerBackoffice:{
+        marginTop: 60,
+        height: 480,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    form: {
+        width: "100%",
+    },
+    button: {
+        marginTop: 15,
+        background: "lightpink",
+        "&:hover": {
+            backgroundColor: "lightpink",
+        },
+    }
+
+}));
+
+export default useStyles;
